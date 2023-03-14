be6a9692f80e164bb02de0c97a6c5fb78c8a5c5b
iamhariombr <hariom080212700@gmail.com>
2023-03-15 04:00:45 +0530

    Completely HTML, JS Code Running.

---
+++ print.js
@@ -0,0 +1,17 @@
+
+//* script allready used in html File.'
+
+// function printDiv() 
+// {
+//   var divToPrint=document.getElementById('DivIdToPrint');
+//   var newWin=window.open('','Print-Window');
+//   newWin.document.open();
+//   newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+//   newWin.document.close();
+//   setTimeout(function(){newWin.close();},10);
+// }
+
+
+
+
+//----------code by github@iamhariombr---------->
